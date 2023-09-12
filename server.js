9091e3ff9e02e2bd6a2543e75fbefbaa0f3325a0
Vo Hoang Son <Vohoangson011218@gmail.com>
2023-09-12 12:06:16 +0700

    edit-Cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const AccountModel = require('./models/account')
 app.use(cookieParser())
 // CORS policy settings 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
